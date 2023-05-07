2eb5d744fa9505b2858547e0537f088f4dcc9f6b
Burak Özdemir <burak-9@live.dk>
2023-05-07 18:59:46 +0200

    drag drop added back

--- app/src/main/java/marp/utilities/IOFunctions.java
+++ app/src/main/java/marp/utilities/IOFunctions.java
@@ -11,12 +11,20 @@ public class IOFunctions {
     IOFunctions() {
 
     }
-    public static List<String> getFiles() throws MalformedURLException{
+
+    public static List<String> getFiles() throws MalformedURLException {
         // directory is resources
         URL dir = DefaultPath.getDefaultPath();
-        return Stream.of(new File(dir.getFile()).listFiles())
+        System.out.println("getDefaultPath: " + dir.toString());
+        List<String> end = Stream.of(new File(dir.getFile()).listFiles())
                 .filter(file -> !file.isDirectory())
                 .map(File::getName)
                 .collect(Collectors.toList());
-    }   
+
+                System.out.println("Available files (" + end.size() + "):");
+                for (String item : end){
+                    System.out.println(item);
+                }
+                return end;
+    }
 }
--- app/src/main/java/marp/view/ChooseMapScene.java
+++ app/src/main/java/marp/view/ChooseMapScene.java
@@ -1,15 +1,25 @@
 package marp.view;
 
+import java.io.File;
+import java.net.URL;
+
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import marp.model.Model;
+import marp.utilities.DefaultPath;
 import marp.view.gui.MapLabel;
 import marp.view.gui.buttons.MapTextButton;
 
@@ -17,19 +27,56 @@ public class ChooseMapScene extends Scene{
     public MapTextButton loadButton;
     private Model model;
 
-    public ChooseMapScene(Model model) {
+    public ChooseMapScene(Model model, View view) {
         super(new VBox());
         this.model = model;
+
 
         // Header
         MapLabel header = new MapLabel("Select a map");
 
         // Listview
         ListView<String> fileList = new ListView<>();
-        fileList.getItems().addAll(model.getFileList());
+        //fileList.getItems().addAll(model.getFileList());
         fileList.setMaxWidth(400);
         fileList.getStylesheets().add("CSS/stylesheet.css");
         fileList.getStyleClass().add("file-list");
+
+        fileList.setOnDragOver(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                if (event.getDragboard().hasUrl()) {
+                    event.acceptTransferModes(TransferMode.COPY);
+                }
+
+                event.consume();
+            }
+        });
+        
+        fileList.setOnDragDropped((DragEvent event) -> {
+            Dragboard dragBoard = event.getDragboard();
+            if (dragBoard.hasUrl()) {
+                System.out.println("Received file: " + dragBoard.getUrl());
+                event.setDropCompleted(true);
+            } else {
+                event.setDropCompleted(false);
+            }
+            event.consume();
+            try {
+                String dragBoardUrl = dragBoard.getUrl().replaceAll("file\\:", "");
+                File file = new File(dragBoardUrl);
+                URL fileToURL = file.toURI().toURL();
+                Model.createModel(fileToURL);
+                view.createNewMapScene();
+                view.setScene(view.getMapScene());
+            } catch (Exception e) {
+                e.printStackTrace();
+                Alert errorMsg = new Alert(AlertType.ERROR);
+                errorMsg.setTitle("Unable to load file!");
+                errorMsg.setContentText("The file you tried to drag and drop could not be loaded! Please try again.");
+                
+                errorMsg.showAndWait();
+            }
+        });
 
         // Load button
         this.loadButton = new MapTextButton("Load a new .osm file...");
